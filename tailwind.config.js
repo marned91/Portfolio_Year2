081d7e159b72a1876d575985592b1de8249aef77
marned91 <152280051+marned91@users.noreply.github.com>
2025-03-25 13:55:40 +0100

    adding tailwind, header, footer

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./**/*.{html,js}', '!./node_modules/**/*'],
+  theme: {
+    extend: {
+      fontFamily: {
+        largeFont: ['Outfit', 'sans-serif'],
+        smallFont: ['Manrope', 'sans-serif'],
+        headerFooter: ['Poppins', 'sans-serif'],
+      },
+      colors: {
+        brand: {
+          light: '#D1DBDD',
+          DEFAULT: '#76979C',
+          dark: '#3C3D40',
+        },
+      },
+      screens: {
+        xs: '460px',
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1536px',
+      },
+      boxShadow: {
+        '3xl': '0 10px 30px rgba(0, 0, 0, 0.5)',
+      },
+    },
+  },
+  plugins: [],
+};
